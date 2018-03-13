675a647d56f6a0a72789692dd619678b96b86410
Woji <lubowicki.w@gmail.com>
2018-03-13 22:35:31 +0100

    upload working

--- modules/handlers.js
+++ modules/handlers.js
@@ -5,9 +5,14 @@ var formidable = require('formidable');
 exports.upload = function(request, response) {
     console.log("Rozpoczynam obsługę żądania upload.");
     var form = new formidable.IncomingForm();
+    form.uploadDir = __dirname + '/../upload/';
+
     form.parse(request, function(error, fields, files) {
-        console.log(error);
-        fs.renameSync(files.upload.path, "./test.png");
+        console.log(files.upload.name);
+        console.log(files.upload.path);
+        console.log(__dirname);
+
+        fs.renameSync(files.upload.path, "test.png");
         response.writeHead(200, {"Content-Type": "text/html"});
         response.write("received image:<br/>");
         response.write("<img src='/show' />");
